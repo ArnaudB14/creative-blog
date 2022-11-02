ac9ebecfe9ca3d1bc10fd213b5e1e2853ed05b7c
ArnaudB14 <arnaud.barbey14@gmail.com>
2022-11-02 15:17:10 +0100

    layout header app.blade + migration post table

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,6 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello-creative', fn() => view('hello-creative'))->name('hello-creative');
+
+Route::get('/', [PageController::class, 'home']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
